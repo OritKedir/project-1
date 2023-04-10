3743639e5a8f8643e266fcef5c84b3f36024e3be
OritKedir <oritkedir36@gmail.com>
2023-04-10 16:11:48 +0300

    updating

--- cost_estimation.cpp
+++ cost_estimation.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-	cout<<"hello world";
-	return 0;
-}
+
+int main() {
+    double current_cost, rate, fraction;
+    int years, choice,i;
+
+    cout << "Enter the current cost of the item: $";
+    cin >> current_cost;
+    cout << "Enter the number of years from now that the item will be purchased: ";
+    cin >> years;
+    cout << "Enter the rate of inflation as a percentage: ";
+    cin >> rate;
