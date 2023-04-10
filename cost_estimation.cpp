e79eb85713b1c6abb07cea285069e240a91e21e9
rabiyatebrahim <rabiyatebrahim@gmail.com>
2023-04-10 07:41:02 -0700

    updating the code

--- cost_estimation.cpp
+++ cost_estimation.cpp
@@ -1,6 +1,26 @@
-#include<iostream>
+#include <iostream>
+#include <iomanip>
 using namespace std;
-int main(){
-	cout<<"hello world";
-	return 0;
+
+int main() {
+    double current_cost, rate, fraction;
+    int years, choice,i;
+
+    cout << "Enter the current cost of the item: $";
+    cin >> current_cost;
+    cout << "Enter the number of years from now that the item will be purchased: ";
+    cin >> years;
+    cout << "Enter the rate of inflation as a percentage: ";
+    cin >> rate;
+
+    fraction = rate / 100.0;
+
+    double adjustedCost[100];
+  adjustedCost[0] = current_cost;
+    double priceDiff[years+1];
+    priceDiff[0] = 0;
+
+    for (int i = 1; i <= years; i++) {
+        adjustedCost[i] = adjustedCost[i-1] *(1 +  fraction);
+        priceDiff[i] = adjustedCost[i] - adjustedCost[i-1];
 }
