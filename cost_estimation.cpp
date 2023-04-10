b5c8efec9bedab058bdc94a094efe7a4080e0577
OritKedir <oritkedir36@gmail.com>
2023-04-10 17:52:58 +0300

    finishing the first code

--- cost_estimation.cpp
+++ cost_estimation.cpp
@@ -12,19 +12,16 @@ int main() {
     cin >> years;
     cout << "Enter the rate of inflation as a percentage: ";
     cin >> rate;
-fraction = rate / 100.0;
-
-    double adjustedCost = current_cost;
-    double priceDiff[years+1];
-    priceDiff[0] = current_cost;
-
-    for (int i = 1; i <= years; i++) {
-        adjustedCost += (adjustedCost * fraction);
-        priceDiff[i] = adjustedCost - priceDiff[i-1];
+ if (choice==1) {
+        cout << "\nYear\tAdjusted Cost\tPrice Difference\n";
+        for (int i = 1; i <= years; i++) {
+            cout << i << "\t$" << adjustedCost[i-1] *(1 +  fraction) << "\t\t$" << priceDiff[i] << endl;
+        }
+    } else if(choice=0) {
+        cout << "\nEnter the year you want to see the adjusted cost for: ";
+        cin >> years;
+        cout << "Adjusted cost in year " << year << ": $" <<  adjustedCost[year] << endl;
     }
-
-    cout << fixed << setprecision(2);
-    cout << "\nEstimated cost of the item after " << years << " years: $" << adjustedCost << endl;
-
-    cout << "\nDo you want to see the cost difference between every year? (1 for Yes, 0 for No): ";
-    cin >> choice;
+   
+    return 0;
+}
