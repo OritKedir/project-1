cdcaf1b74ef34785fba62d66421ab23e3a7743ef
OritKedir <oritkedir36@gmail.com>
2023-04-10 18:55:01 +0300

    updating already existing code

--- cost_estimation.cpp
+++ cost_estimation.cpp
@@ -14,20 +14,33 @@ int main() {
     cin >> rate;
 fraction = rate / 100.0;
 
-    double adjustedCost = current_cost;
+    double adjustedCost[100] ;
+    adjustedCost[0]=current_cost;
     double priceDiff[years+1];
-    priceDiff[0] = current_cost;
+    priceDiff[0] = 0;
 
     for (int i = 1; i <= years; i++) {
-        adjustedCost += (adjustedCost * fraction);
-        priceDiff[i] = adjustedCost - priceDiff[i-1];
+        adjustedCost[i] = adjustedCost[i-1] *(1+ fraction);
+        priceDiff[i] = adjustedCost[i] - adjustedCost[i-1];
     }
 
     cout << fixed << setprecision(2);
-    cout << "\nEstimated cost of the item after " << years << " years: $" << adjustedCost << endl;
+    cout << "\nEstimated cost of the item after " << years << " years: $" << adjustedCost[years] << endl;
 
     cout << "\nDo you want to see the cost difference between every year? (1 for Yes, 0 for No): ";
     cin >> choice;
      if (choice==1) {
         cout << "\nYear\tAdjusted Cost\tPrice Difference\n";
         for (int i = 1; i <= years; i++) {
+ cout << i << "\t$" << adjustedCost[i-1] *(1 +  fraction) << "\t\t$" << priceDiff[i] << endl;
+        }
+    } else if(choice==0) {
+        cout << "\nEnter the year you want to see the adjusted cost for: ";
+        cin >> years;
+        cout << "Adjusted cost in year " << years << ": $" <<  adjustedCost[years] << endl;
+    }
+    else{
+      cout<<"Invalid input";
+  }
+    return 0;
+}
